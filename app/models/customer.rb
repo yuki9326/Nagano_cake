038f3615cde81af64f6e47833ef12d88eac61474
yuki9326 <ukey.0613@gmail.com>
2021-06-27 17:56:35 +0000

    [Add]顧客の会員登録画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many:addresses
+         has_many:orders
+         has_many:cart_items
+         has_many:items,through: :cart_items
 end
