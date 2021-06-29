4c7deccdd301b6ad1889160e0b14b0316e800f5b
yuki9326 <ukey.0613@gmail.com>
2021-06-29 14:54:49 +0000

    [Add]管理者側会員一覧ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,7 +26,10 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
     redirect_to admin_item_path(@item.id)
+  else render :edit
+    end
   end
 
   private
