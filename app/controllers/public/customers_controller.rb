0be962eceabd02692df18bf3efee5fa8dfed01b1
yuki9326 <ukey.0613@gmail.com>
2021-07-08 06:52:51 +0000

    [Finish]顧客の会員情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,15 +5,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-    @customer
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id)
+    else render :edit
+    end
   end
 
   def confirm
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def customer_params
+  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :emai)
   end
 end
